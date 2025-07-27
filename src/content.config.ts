b7d993807bd012840e10ece351b501398ca33b58
Richard Sengkey <richardsimonb@gmail.com>
2025-07-27 14:03:45 +0700

    create content config file

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+    loader: glob({pattern: "**/*.md", base: "./src/blog"}),
+    schema: z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        date: z.date(),
+        tags: z.array(z.string()).optional(),
+    }),
+});
+
+export const collections = { blog };
